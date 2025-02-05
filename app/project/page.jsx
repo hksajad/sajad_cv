6511d91eaf07b12b1ac8d6a35c38fb5b0667984b
sajjad <sajjadhasanikakh06@gmail.com>
2025-02-05 20:07:16 +0330

    add

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -1,19 +1,46 @@
-import Link from 'next/link'
-import React from 'react'
-
+import Link from "next/link";
+import React from "react";
+import './index.css'
 const Page = () => {
   return (
-    <div className='projects'>
+    <main>
+      <div className="projects">
         <div className="title_projects flex flex-col justify-center items-center mt-[50px] md:mt-[70px]">
-            <h1 className='text-[50px]'>پروژه های من</h1>
-            <div className="links_project flex gap-2 mt-4 text-[17px]">
-                <Link href="/" className='text-orange-300'>صفحه اصلی</Link>
-                /
-                <span className='text-[#999]'>پروژه ها</span>
-            </div>
+          <h1 className="text-[50px]">پروژه های من</h1>
+          <div className="links_project flex gap-2 mt-4 text-[17px]">
+            <Link href="/" className="text-orange-300">
+              صفحه اصلی
+            </Link>
+            /<span className="text-[#999]">پروژه ها</span>
+          </div>
         </div>
-    </div>
-  )
-}
+        <div className="content_project flex justify-between items-center flex-wrap mt-24 max-lg:gap-5 max-md:mt-16">
+          <div className="img_project">
+            <img src="/img/IT_APP.png" alt="resume_it" />
+          </div>
+          <div className="img_project">
+            <img src="/img/cosmetic.png" alt="resume_it" />
+          </div>
+          <div className="img_project">
+            <img src="/img/slg.png" alt="resume_it" />
+          </div>
+          <div className="img_project lg:mt-10">
+            <img src="/img/fitness.png" alt="resume_it" />
+          </div>
+          <div className="img_project lg:mt-10">
+            <img src="/img/shop.png" alt="resume_it" />
+          </div>
+          <div className="img_project lg:mt-10">
+            <img src="/img/shoop.png" alt="resume_it" />
+          </div>
+        </div>
+        {/* footer */}
+      <footer className="my-5 text-center border-t-2 border-[#999]">
+        <h1 className="text-[20px] font-bold text-[#999] mt-5">طراحی شده توسط سجاد حسنی کاخ</h1>
+      </footer>
+      </div>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
--- components/layout/Hero_page/Hero.jsx
+++ components/layout/Hero_page/Hero.jsx
@@ -2,7 +2,7 @@ import Header from "@/components/module/Header/Header";
 import Navbar from "@/components/module/Navbar/Navbar";
 import About from "@/components/template/About/About";
 import Work from "@/components/template/Work/Work";
-import './Hero.css'
+import "./Hero.css";
 import Resume from "@/components/template/resume/Resume";
 import Degree from "@/components/template/degree/Degree";
 import Contact from "@/components/template/contact/Contact";
@@ -12,20 +12,21 @@ const Hero = () => {
       {/* start navbar */}
       <Navbar />
       {/* content fixed page hero */}
-      <Header/>
+      <Header />
       {/* section page */}
       <div className="section_page bg-[#303030] w-full lg:w-[60%] rounded-md lg:h-[465px]  lg:overflow-y-auto">
-        <About/>
-        <Work/>
-        <Resume/>
-        <Degree/>
-        <Contact/>
+        <About />
+        <Work />
+        <Resume />
+        <Degree />
+        <Contact />
         {/* footer */}
-      <footer className="my-5 text-center">
-        <h1 className="text-[20px] font-bold text-[#999]">طراحی شده توسط سجاد حسنی کاخ</h1>
-      </footer>
+        <footer className="my-5 text-center">
+          <h1 className="text-[20px] font-bold text-[#999]">
+            طراحی شده توسط سجاد حسنی کاخ
+          </h1>
+        </footer>
       </div>
-      
     </div>
   );
 };
